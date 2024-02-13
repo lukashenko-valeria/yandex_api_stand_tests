54504e79a298359831167dc61380b27d250733f8
Валерия Лукашенко <vlukashenko@MacBook-Air-Valeria.local>
2024-02-13 21:07:46 +0500

    My first commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://b1e7edef-97d2-40a7-b3eb-93375198d567.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
---
+++ sender_stand_request.py
@@ -0,0 +1,35 @@
+import configuration
+import requests
+import data
+
+def get_docs():
+    return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
+
+def get_logs():
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH,
+                        params = {"count": 20})
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # подставляем полный url
+                         json=body,  # тут тело
+                         headers=data.headers)  # а здесь заголовки
+
+def post_products_kits(products_ids):
+    return requests.post(configuration.URL_SERVICE + configuration.PRODUCTS_KITS_PATH, json=products_ids,
+                             headers=data.headers)
+
+# эта функция меняет значения в параметре firstName
+def get_user_body(first_name):
+    # копирование словаря с телом запроса из файла data, чтобы не потерять данные в исходном словаре
+    current_body = data.user_body.copy()
+    # изменение значения в поле firstName
+    current_body["firstName"] = first_name
+    # возвращается новый словарь с нужным значением firstName
+    return current_body
+
+
+
